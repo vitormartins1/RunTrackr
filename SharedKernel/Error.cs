2070d26e7b4728810cbe1b430fa730be3e72a2c9
Vitor Martins <vitormartins.one@gmail.com>
2023-12-08 00:25:08 -0300

    Adiciona arquivos de solução;

--- SharedKernel/Error.cs
+++ SharedKernel/Error.cs
@@ -6,4 +6,6 @@ public sealed record Error(string Code, string? Description = null)
     public static readonly Error NullValue = new("Error.NullValue", "Null value was provided");
 
     public static implicit operator Result(Error error) => Result.Failure(error);
+
+    public Result ToResult() => Result.Failure(this);
 }
